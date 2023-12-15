9bb23ed5ba75b030290fcd22cac8ae6491257105
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-15 18:15:50 +0600

    register and login page working

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
 import registerImage from "../../public/assets/others/authentication2.png";
 const Login = () => {
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -8,14 +16,15 @@ const Login = () => {
             <img src={registerImage} alt="" />
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
+                  name="email"
                   type="email"
-                  placeholder="email"
+                  placeholder="Email"
                   className="input input-bordered"
                   required
                 />
@@ -25,8 +34,9 @@ const Login = () => {
                   <span className="label-text">Password</span>
                 </label>
                 <input
+                  name="password"
                   type="password"
-                  placeholder="password"
+                  placeholder="Password"
                   className="input input-bordered"
                   required
                 />
@@ -37,9 +47,12 @@ const Login = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-outline btn-warning">Login</button>
+              <input className="btn btn-outline btn-warning" type="submit" value="Login" />
               </div>
             </form>
+            <div className="mx-auto">
+                <p className="text-[#D1A054] text-[20px] mb-4">New here? <Link to="/register"><button className="">Create a New Account</button></Link></p>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
 import registerImage from "../../public/assets/others/authentication2.png";
 const Register = () => {
+  const handleRegister = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const image = form.image.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name,image, email, password);
+  };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -8,14 +18,39 @@ const Register = () => {
             <img src={registerImage} alt="" />
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleRegister} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Image URL</span>
+                </label>
+                <input
+                  name="image"
+                  type="text"
+                  placeholder="Image URL"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
+                  name="email"
                   type="email"
-                  placeholder="email"
+                  placeholder="Email"
                   className="input input-bordered"
                   required
                 />
@@ -25,8 +60,9 @@ const Register = () => {
                   <span className="label-text">Password</span>
                 </label>
                 <input
+                  name="password"
                   type="password"
-                  placeholder="password"
+                  placeholder="Password"
                   className="input input-bordered"
                   required
                 />
@@ -37,9 +73,21 @@ const Register = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-outline btn-warning">Register</button>
+                <input
+                  className="btn btn-outline btn-warning"
+                  type="submit"
+                  value="Register"
+                />
               </div>
             </form>
+            <div className="mx-auto">
+              <p className="text-[#D1A054] text-[20px] mb-4">
+                Already registered?{" "}
+                <Link to="/login">
+                  <button className="">Go to Login</button>
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
