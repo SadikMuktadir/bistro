9816134792ea0561cd6eed17a75b12ed8a065bc4
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-21 17:18:34 +0600

    jwt apply in ui

--- src/Components/AuthProvider.jsx
+++ src/Components/AuthProvider.jsx
@@ -1,9 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { app } from "../../public/firebase.config";
+import useAxiosPublic from "../Hooks/useAxiospublic";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
+  const axiosPublic = useAxiosPublic();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
@@ -17,6 +19,18 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      if(currentUser){
+        const userInfo ={email:currentUser.email};
+        axiosPublic.post('/jwt',userInfo)
+        .then(res=>{
+          if(res.data.token){
+            localStorage.setItem('access-token',res.data.token);
+          }
+        })
+      }
+      else{
+        localStorage.removeItem('access-token');
+      }
       setLoading(false);
     });
     return () => {
